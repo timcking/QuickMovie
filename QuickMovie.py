cdd327c883ded89976aac19018407e4b4192dfc8
Tim King <timcking@gmail.com>
2013-03-17 00:42:34 -0700

    Getting basic person info

--- QuickMovie.py
+++ QuickMovie.py
@@ -4,6 +4,7 @@ from wx import xrc
 
 class MyApp(wx.App):
     dictTitles = {}
+    dictCast = {}
 
     def OnInit(self):
         self.res = xrc.XmlResource('QuickMovie.xrc')
@@ -31,24 +32,30 @@ class MyApp(wx.App):
         self.frame.Bind(wx.EVT_BUTTON, self.OnClose, id=xrc.XRCID('wxID_EXIT'))
         self.frame.Bind(wx.EVT_CLOSE, self.OnExitApp)
         self.Bind(wx.EVT_BUTTON, self.OnSearchClick, self.btnSearch)
-        self.cboTitle.Bind(wx.EVT_KEY_DOWN, self.OnTitleChange)        
+        self.cboTitle.Bind(wx.EVT_KEY_DOWN, self.OnTitleChange)
         self.cboTitle.Bind(wx.EVT_COMBOBOX, self.OnComboTitles, id=xrc.XRCID('cboTitle'))
+        self.listCast.Bind(wx.EVT_LISTBOX, self.OnListCast, id=xrc.XRCID('listCast'))
         
         self.statusBar.SetStatusText('Ready')
         self.frame.Show()
         
     def get_movie(self, movie_id):
-        ia = imdb.IMDb()
+        # self.cboTitle.Clear()
+        myCursor= wx.StockCursor(wx.CURSOR_WAIT)
+        self.frame.SetCursor(myCursor)
+        self.statusBar.SetStatusText('Searching ...')
         
+        ia = imdb.IMDb()
         s_result = ia.get_movie(movie_id)
+        
         print ("movie_id: %s" % (movie_id))
         # TODO: Cast, check for each null
-        # TODO: Merge with search_movie
-        print s_result['director'][0]
-        self.txtDirector.SetValue('%s' % s_result['director'][0])
-        self.txtYear.SetValue('%s' % s_result['year'])
-        self.txtRunTime.SetValue('%s min' % s_result['runtime'][0])
-        self.txtPlot.SetValue('%s' % s_result['plot outline'])
+        self.load_fields(s_result)
+        
+        # Set mousepointer to normal
+        myCursor= wx.StockCursor(wx.CURSOR_ARROW)
+        self.frame.SetCursor(myCursor)            
+        self.statusBar.SetStatusText('Ready')    
 
     def search_movie (self, title):
         myCursor= wx.StockCursor(wx.CURSOR_WAIT)
@@ -69,26 +76,31 @@ class MyApp(wx.App):
             self.dictTitles[index_count] = title.movieID
             index_count += 1                    
         
-        # For now, only interested in the first result
+        # For now, only display in the first result
         movie = s_result[0]
         ia.update(movie)
         
-        self.cboTitle.SetValue('%s' % movie['title'])
-        self.txtYear.SetValue('%s' % movie['year'])
-        self.txtDirector.SetValue('%s' % movie['director'][0])
-        self.txtRunTime.SetValue('%s min' % movie['runtime'][0])
-        self.txtPlot.SetValue('%s' % movie['plot outline'])
-        
-        for item in movie['cast']:
-            self.listCast.Append('%s' % item['name'])
-            
-        # self.listCast.SetSelection(0)
+        self.load_fields(movie)
             
         # Set mousepointer to normal
         myCursor= wx.StockCursor(wx.CURSOR_ARROW)
         self.frame.SetCursor(myCursor)            
         self.statusBar.SetStatusText('Ready')    
         
+    def load_fields(self, results):
+        self.cboTitle.SetValue('%s' % results['title'])
+        self.txtYear.SetValue('%s' % results['year'])
+        self.txtDirector.SetValue('%s' % results['director'][0])
+        self.txtRunTime.SetValue('%s min' % results['runtime'][0])
+        self.txtPlot.SetValue('%s' % results['plot outline'])
+        
+        index_count = 0
+        for person in results['cast']:
+            self.listCast.Append('%s' % person['name'])
+            # Save for when cast is clicked
+            self.dictCast[index_count] = person.personID
+            index_count += 1                    
+            
     def clear_fields(self):
         self.txtDirector.SetValue('')
         self.txtRunTime.SetValue('')
@@ -112,10 +124,16 @@ class MyApp(wx.App):
         event.Skip()    
         
     def OnComboTitles(self, event):
-        # TODO: change movie info in form when this is clicked
         selected = self.cboTitle.GetSelection()
         movieID = self.dictTitles[selected]
+        self.clear_fields()
         self.get_movie(movieID)
+        
+    def OnListCast(self, event):
+        selected = self.listCast.GetSelection()
+        personID = self.dictCast[selected]
+        print personID
+        # TODO
                  
     def OnClose(self, evt):
         self.Exit()
