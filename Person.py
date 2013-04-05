2d19a49db81b697a2c07abe8276a7d52d60ae48b
Tim King <timcking@gmail.com>
2013-04-04 18:02:06 -0700

    Clean up

--- Person.py
+++ Person.py
@@ -1,38 +1,34 @@
 import wx
 from wx import xrc
-from wx.lib.dialogs import ScrolledMessageDialog 
+from wx.lib.dialogs import ScrolledMessageDialog
 import urllib
 from cStringIO import StringIO
 
 class Person(wx.Frame):
     dictTrivia = {}
-    
+
     def __init__(self, s_result):
         self.res = xrc.XmlResource('Person.xrc')
         self.init_frame(s_result)
-        
+
     def init_frame(self, s_result):
         self.frame = self.res.LoadFrame(None, 'framePerson')
-        
+
         # Bind Controls
         self.lblName = xrc.XRCCTRL(self.frame, 'lblName')
-        self.bmpPic = xrc.XRCCTRL(self.frame, 'bmpPic') 
+        self.bmpPic = xrc.XRCCTRL(self.frame, 'bmpPic')
         self.txtBirthName = xrc.XRCCTRL(self.frame, 'txtBirthName')
         self.txtBirthDate = xrc.XRCCTRL(self.frame, 'txtBirthDate')
         self.txtBirthPlace = xrc.XRCCTRL(self.frame, 'txtBirthPlace')
         self.txtBio = xrc.XRCCTRL(self.frame, 'txtBio')
         self.listTrivia = xrc.XRCCTRL(self.frame, 'listTrivia')
         self.lblName.SetLabel("%s" % s_result['name'])
-        
+
         # Bind Events
         self.frame.Bind(wx.EVT_BUTTON, self.OnClose, id=xrc.XRCID('wxID_CLOSE'))
         self.frame.Bind(wx.EVT_LISTBOX, self.OnListTriviaClick, id=xrc.XRCID('listTrivia'))
-        
-        # TODO, display headshot, see wxPython book
-        # self.bmpPic.SetBitmap(wx.Bitmap('Veronica.jpg'))
-        # Below does not work
-        # self.bmpPic.SetBitmap(wx.Bitmap(s_result['headshot']))
-        
+
+        # Any of these can be null
         try:
             self.txtBirthName.SetValue('%s' % s_result['birth name'])
         except Exception, e:
@@ -50,39 +46,37 @@ class Person(wx.Frame):
         except Exception, e:
             print "mini biography not found"
         try:
-            print s_result['headshot']
             fp = urllib.urlopen(s_result['headshot'])
             data = fp.read()
             fp.close()
-            img = wx.ImageFromStream(StringIO(data))            
-            bmp = wx.BitmapFromImage(img)
-            self.bmpPic.SetBitmap(bmp)            
+            img = wx.ImageFromStream(StringIO(data))
+            self.bmpPic.SetBitmap(wx.BitmapFromImage(img))
         except Exception, e:
-            print "headshot not found"        
-        try:           
+            print "headshot not found"
+        try:
             index_count = 0
             for item in s_result['trivia']:
                 self.listTrivia.Append('%s' % item)
                 # Save for when trivia is clicked
                 self.dictTrivia[index_count] = item
-                index_count += 1                    
+                index_count += 1
         except Exception, e:
-            print "trivia not found"             
-            
-        self.frame.Show()        
-    
+            print "trivia not found"
+
+        self.frame.Show()
+
     def OnListTriviaClick(self, evt):
         selected = self.listTrivia.GetSelection()
         trivia = self.dictTrivia[selected]
-        
+
         dialog = ScrolledMessageDialog (self.frame, trivia, 'Trivia', pos=wx.DefaultPosition, size=(450, 250))
-        
+
         # Change the font from 8 (default) to 10
-        children = dialog.GetChildren() 
-        textCtrl = children[0] 
-        textCtrl.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False)) 
-        
-        result = dialog.ShowModal()         
-        
+        children = dialog.GetChildren()
+        textCtrl = children[0]
+        textCtrl.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False))
+
+        result = dialog.ShowModal()
+
     def OnClose(self, evt):
-        self.frame.Destroy()    
+        self.frame.Destroy()
