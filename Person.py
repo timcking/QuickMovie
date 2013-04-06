3a7b919ee6092a842c320384a2d0f908cdb75ef8
Tim King <timcking@gmail.com>
2013-04-05 19:04:20 -0700

    Added death info

--- Person.py
+++ Person.py
@@ -18,8 +18,8 @@ class Person(wx.Frame):
         self.lblName = xrc.XRCCTRL(self.frame, 'lblName')
         self.bmpPic = xrc.XRCCTRL(self.frame, 'bmpPic')
         self.txtBirthName = xrc.XRCCTRL(self.frame, 'txtBirthName')
-        self.txtBirthDate = xrc.XRCCTRL(self.frame, 'txtBirthDate')
-        self.txtBirthPlace = xrc.XRCCTRL(self.frame, 'txtBirthPlace')
+        self.txtBirth = xrc.XRCCTRL(self.frame, 'txtBirth')
+        self.txtDeath = xrc.XRCCTRL(self.frame, 'txtDeath')
         self.txtBio = xrc.XRCCTRL(self.frame, 'txtBio')
         self.listTrivia = xrc.XRCCTRL(self.frame, 'listTrivia')
         self.lblName.SetLabel("%s" % s_result['name'])
@@ -34,13 +34,13 @@ class Person(wx.Frame):
         except Exception, e:
             print "birth name not found"
         try:
-            self.txtBirthDate.SetValue('%s' % s_result['birth date'])
+            self.txtBirth.SetValue('%s, %s' % (s_result['birth date'], s_result['birth notes']))
         except Exception, e:
-            print "birth date not found"
+            print "birth date or place not found"
         try:
-            self.txtBirthPlace.SetValue('%s' % s_result['birth notes'])
+            self.txtDeath.SetValue('%s, %s' % (s_result['death date'], s_result['death notes']))
         except Exception, e:
-            print "birth notes not found"
+            print "death info not found"
         try:
             self.txtBio.SetValue('%s' % s_result['mini biography'][0])
         except Exception, e:
